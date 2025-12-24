cf3ea5829e67d37874effe1d7654dd8cf1b6a5ab
TrungNP <trungyn122@gmail.com>
2025-12-24 14:27:08 +0700

    Ghi chú nội dung bạn thay đổi ở đây

---
+++ src/main/java/com/arrowhitech/tts/library/TTS12_25/Tts1225Application.java
@@ -0,0 +1,13 @@
+package com.arrowhitech.tts.library.TTS12_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tts1225Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tts1225Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/arrowhitech/tts/library/TTS12_25/Tts1225ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrowhitech.tts.library.TTS12_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tts1225ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
