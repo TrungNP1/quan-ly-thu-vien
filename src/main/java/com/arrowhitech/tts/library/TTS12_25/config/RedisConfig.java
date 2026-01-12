7c52cceb3f6731033c3d08228b715f21f5c54814
Tuphungvan <cube438126@gmail.com>
2026-01-12 09:16:20 +0700

    Config of redis

---
+++ src/main/java/com/arrowhitech/tts/library/TTS12_25/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.arrowhitech.tts.library.TTS12_25.config;
+
+import java.time.Duration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10)) 
+                .serializeValuesWith(SerializationPair.fromSerializer(new JdkSerializationRedisSerializer()));
+    }
+}
